5f18e9a7db60b7e4bd5d8f102f27bf1a245689f6
Alonso Lira Sifuentes <alonsolira318@gmail.com>
2024-08-08 20:17:41 -0600

    ejercicios

--- 07082024/01VariablesNumericas.js
+++ 07082024/01VariablesNumericas.js
@@ -1,11 +1,12 @@
-/*
+/*CONVERTIR METROS A KILOMETROS--------------
 var metros =1250;
 
 var kilometros = metros/1000;
 
 console.log("en kilometros es "+kilometros);
 */
-/*
+
+/*CALCULAR EL INDICE DE MASA CORPORAL--------------
 var kg = prompt ("Ingresar peso");
 var estatura = prompt("ingresa tu estatura");
 var IMC = kg/estatura*estatura;
@@ -13,7 +14,28 @@ var IMC = kg/estatura*estatura;
 console.log("Tu indice de masa corporal es "+ IMC);
 */
 
+/*CONVERTIR GRADOS CEL A FA----------------------
 var gradosCel = prompt("ingresa grados cel");
 var gradosfa = 9/5*+gradosCel+32;
 
 console.log("la conversion a grados fa es "+ gradosfa);
+*/
+
+/*OPERACIONES ARITMETICAS-------------------
+var numero1 = prompt("Ingresa el primer numero");
+var numero2 = prompt("Ingresa el segundo numero");
+var suma = parseInt(numero1)+parseInt(numero2);
+var resta = numero1-numero2;
+var multiplicacion = numero1*numero2;
+var division = numero1/numero2;
+console.log("la suma es: "+suma);
+console.log("La resta es :"+resta);
+console.log("La multiplicacion es: "+multiplicacion);
+console.log("La division es: "+division);
+*/
+
+/*Cadena de texto que devuelve la longitud
+var string =prompt("Ingresa el texto");
+console.log("El texto es: "+ string+" con una longitud de "+string.length);
+*/
+
